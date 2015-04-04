925487b55fcdc66a8e24b057e4a2cd83d6589b8d
Trevor Shannon <trevor.j.shannon@gmail.com>
2015-04-04 09:08:56 -0700

    updated tile size

--- tiler.py
+++ tiler.py
@@ -2,10 +2,10 @@ import itertools
 from matplotlib import pyplot
 import numpy
 
-_TILE_WIDTH = 6.0  # in
-_TILE_HEIGHT = 3.0  # in
-_PLANE_WIDTH = 32.0  # in
-_PLANE_HEIGHT = 64.0  # in
+_TILE_WIDTH = 7.9375  # in
+_TILE_HEIGHT = 4.0  # in
+_PLANE_WIDTH = 60.0  # in
+_PLANE_HEIGHT = 68.0  # in
 _OVERLAP = 0.5
 
 def get_spaced_points(unit_dim, plane_dim, fixed_point=0):
@@ -16,7 +16,8 @@ def get_spaced_points(unit_dim, plane_dim, fixed_point=0):
   vertices.append(plane_dim)
   return vertices
 
-x_pos = (_PLANE_WIDTH - _TILE_WIDTH) / 2.0 + 1
+#x_pos = (_PLANE_WIDTH - _TILE_WIDTH) / 2.0 + 2
+x_pos = 2.25
 y_pos = 0
 x_shift = _TILE_WIDTH * _OVERLAP
 
@@ -31,6 +32,8 @@ for j in range(len(y_vertices) - 1):
   # compute x tile positions for this row
   x_vertices = get_spaced_points(_TILE_WIDTH, _PLANE_WIDTH,
                                  x_pos + x_shift * (j%2))
+  if j == 0 or j == 1:
+    print 'x positions (in): ', x_vertices
   axes.plot(x_vertices, [y_vertices[j]] * len(x_vertices), 'k')
   axes.plot(x_vertices, [y_vertices[j + 1]] * len(x_vertices), 'k')
   for x_vertex in x_vertices:
@@ -43,4 +46,5 @@ axes.plot([0, _PLANE_WIDTH, _PLANE_WIDTH, 0, 0],
 pyplot.axis('equal')
 axes.set_ylim(-0.05 * _PLANE_HEIGHT, 1.05 * _PLANE_HEIGHT)
 axes.set_xlim(-0.05 * _PLANE_WIDTH, 1.05 * _PLANE_WIDTH)
+
 pyplot.show()
